9d783c5ea1a9feb491cf9836de3a6147c9d1b106
mukies <muk.yess@gmai.com>
2024-07-10 22:14:24 +0545

    minor changes in index.js

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ const blogRoute = require("./routes/blog.r");
 require("./config/passport-setup");
 const googleAuthRoute = require("./routes/googleAuth.r");
 const partnerRoute = require("./routes/partner.r");
-const testModel = require("./test/test.m");
 // const testModel = require("./test/test.m");
 // const {
 //   generateTokenAndSetCookie,
@@ -65,80 +64,6 @@ app.use("/api/contact-us", contactUsRoute);
 app.use("/api/category", state_and_activities_route);
 app.get("/", (req, res) => {
   res.status(200).json({ success: true, message: "server is ok." });
-});
-
-app.post("/add", async (req, res) => {
-  const { username, age, name } = req.body;
-  try {
-    const isExist = await testModel.find({ username });
-
-    if (isExist.length > 0)
-      return res
-        .status(400)
-        .json({ success: false, message: "Username already exist" });
-
-    const newData = new testModel({ username, age: Number(age), name });
-    await newData.save();
-    res.status(200).json({ success: true, newData, message: "created" });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "server error." });
-  }
-});
-
-app.put("/update/:id", async (req, res) => {
-  const { username, age, name } = req.body;
-  const { id } = req.params;
-  try {
-    const isExist = await testModel.findOne({ username, _id: { $ne: id } });
-    if (isExist) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Username already exists" });
-    }
-
-    const updatedData = await testModel.findByIdAndUpdate(
-      id,
-      {
-        username,
-        age,
-        name,
-      },
-      { new: true }
-    );
-    res.status(200).json({ success: true, updatedData, message: "updated" });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "server error." });
-  }
-});
-
-app.get("/get", async (req, res) => {
-  try {
-    const data = await testModel.find();
-
-    res.status(200).json({ success: true, data, message: "fetched" });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "server error." });
-  }
-});
-app.get("/single/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const data = await testModel.findById(id);
-
-    res.status(200).json({ success: true, data, message: "fetched" });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "server error." });
-  }
-});
-app.delete("/delete/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    await testModel.findByIdAndDelete(id);
-
-    res.status(200).json({ success: true, message: "deleted" });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "server error." });
-  }
 });
 
 app.use("*", (req, res) => {
