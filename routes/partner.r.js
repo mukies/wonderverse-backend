60959cc1825b4e3eb8cab433150b793dc96e7f1c
mukies <muk.yess@gmai.com>
2024-07-12 17:20:32 +0545

    minor changes in partner route

--- routes/partner.r.js
+++ routes/partner.r.js
@@ -5,13 +5,14 @@ const {
   logoutPartner,
   loggedInPartner,
 } = require("../controllers/partner.c");
+const { partnerProtection } = require("../middlewares/partnerProtection");
 
 const router = require("express").Router();
 
 router.post("/register", registerPartner);
 router.post("/login", loginPartner);
 router.post("/logout", logoutPartner);
-router.get("/loggedin-user-data", loggedInPartner);
+router.get("/loggedin-user-data", partnerProtection, loggedInPartner);
 router.put("/verify/:uid", verifyPartner);
 
 module.exports = router;
